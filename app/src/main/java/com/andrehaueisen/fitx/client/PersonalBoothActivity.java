c52343b74576ef368f39e549e397228dd783a65c
Andre Haueisen Batista de Lima <andrehaueisen@gmail.com>
2017-04-06 17:01:18 -0300

    change: simplified code on ExpandableBoothAdapter

--- app/src/main/java/com/andrehaueisen/fitx/client/PersonalBoothActivity.java
+++ app/src/main/java/com/andrehaueisen/fitx/client/PersonalBoothActivity.java
@@ -10,15 +10,11 @@ import android.support.v7.widget.Toolbar;
 
 import com.andrehaueisen.fitx.Constants;
 import com.andrehaueisen.fitx.R;
-import com.andrehaueisen.fitx.Utils;
 import com.andrehaueisen.fitx.client.adapters.ExpandableBoothAdapter;
-import com.andrehaueisen.fitx.models.PersonalDetailed;
 import com.andrehaueisen.fitx.models.PersonalFitClass;
-import com.andrehaueisen.fitx.models.PersonalResume;
 import com.andrehaueisen.fitx.models.PersonalTrainer;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by andre on 10/10/2016.
@@ -29,7 +25,6 @@ public class PersonalBoothActivity extends AppCompatActivity{
     private ArrayList<PersonalTrainer> mPersonalTrainers;
     private PersonalFitClass mPersonalFitClass;
 
-    private ExpandableBoothAdapter mExpandableBoothAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,7 +40,6 @@ public class PersonalBoothActivity extends AppCompatActivity{
 
         Intent intent = getIntent();
         mPersonalTrainers = intent.getParcelableArrayListExtra(Constants.PERSONAL_LIST_EXTRA_KEY);
-
         mPersonalFitClass = intent.getParcelableExtra(Constants.FIT_CLASS_EXTRA_KEY);
 
         setAdapter();
@@ -58,38 +52,9 @@ public class PersonalBoothActivity extends AppCompatActivity{
         trainersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         trainersRecyclerView.setHasFixedSize(true);
 
-        List<PersonalResume> personalResumes;
-
-        personalResumes = configureExpandableAdapterInfo();
-
-        mExpandableBoothAdapter = new ExpandableBoothAdapter(personalResumes, mPersonalTrainers, this, mPersonalFitClass);
-        trainersRecyclerView.setAdapter(mExpandableBoothAdapter);
+        ExpandableBoothAdapter expandableBoothAdapter = new ExpandableBoothAdapter( mPersonalTrainers, this, mPersonalFitClass);
+        trainersRecyclerView.setAdapter(expandableBoothAdapter);
     }
 
-    private List<PersonalResume> configureExpandableAdapterInfo(){
 
-
-        List<PersonalResume> personalResumes = new ArrayList<>();
-
-        for(PersonalTrainer personalTrainer : mPersonalTrainers) {
-
-            List<PersonalDetailed> personalDetaileds = new ArrayList<>();
-
-            PersonalDetailed personalDetailed = new PersonalDetailed();
-            personalDetailed.setCref(personalTrainer.getCref());
-            personalDetailed.setReviewCounter(personalTrainer.getReviewCounter());
-
-            personalDetaileds.add(personalDetailed);
-
-            String name = personalTrainer.getName();
-            float grade = personalTrainer.getGrade();
-            String key = Utils.encodeEmail(personalTrainer.getEmail());
-
-            PersonalResume personalResume = new PersonalResume(name, grade, personalDetaileds, key);
-
-            personalResumes.add(personalResume);
-        }
-
-        return personalResumes;
-    }
 }
--- app/src/main/java/com/andrehaueisen/fitx/client/adapters/ExpandableBoothAdapter.java
+++ app/src/main/java/com/andrehaueisen/fitx/client/adapters/ExpandableBoothAdapter.java
@@ -1,6 +1,7 @@
 package com.andrehaueisen.fitx.client.adapters;
 
-import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,91 +13,111 @@ import com.andrehaueisen.fitx.Utils;
 import com.andrehaueisen.fitx.client.PersonalBoothActivity;
 import com.andrehaueisen.fitx.client.ScheduleConfirmation;
 import com.andrehaueisen.fitx.client.firebase.FirebaseProfileImageCatcher;
-import com.andrehaueisen.fitx.models.PersonalDetailed;
 import com.andrehaueisen.fitx.models.PersonalFitClass;
-import com.andrehaueisen.fitx.models.PersonalResume;
 import com.andrehaueisen.fitx.models.PersonalTrainer;
-import com.bignerdranch.expandablerecyclerview.ChildViewHolder;
-import com.bignerdranch.expandablerecyclerview.ExpandableRecyclerAdapter;
-import com.bignerdranch.expandablerecyclerview.ParentViewHolder;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by andre on 12/8/2016.
  */
 
-public class ExpandableBoothAdapter extends ExpandableRecyclerAdapter<PersonalResume, PersonalDetailed, ExpandableBoothAdapter.PersonalResumeParentHolder, ExpandableBoothAdapter.PersonalDetailedChildHolder> {
+public class ExpandableBoothAdapter extends RecyclerView.Adapter<ExpandableBoothAdapter.PersonalDetailedHolder> {
 
     private PersonalBoothActivity mActivity;
     private List<PersonalTrainer> mPersonalTrainers;
     private FirebaseProfileImageCatcher mFirebaseProfileImageCatcher;
     private PersonalFitClass mPersonalFitClass;
 
-    public ExpandableBoothAdapter(@NonNull List<PersonalResume> parentList, List<PersonalTrainer> personalTrainers, PersonalBoothActivity activity, PersonalFitClass personalFitClass) {
-        super(parentList);
+    public ExpandableBoothAdapter(List<PersonalTrainer> personalTrainers, PersonalBoothActivity activity, PersonalFitClass personalFitClass) {
 
         mActivity = activity;
         mPersonalTrainers = personalTrainers;
         mPersonalFitClass = personalFitClass;
     }
 
-    @NonNull
     @Override
-    public PersonalResumeParentHolder onCreateParentViewHolder(@NonNull ViewGroup parentViewGroup, int viewType) {
+    public PersonalDetailedHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View parentView = LayoutInflater.from(mActivity).inflate(R.layout.item_personal_resume_parent, parentViewGroup, false);
-        PersonalResumeParentHolder holder = new PersonalResumeParentHolder(parentView);
+        View parentView = LayoutInflater.from(mActivity).inflate(R.layout.item_personal_resume_parent, parent, false);
+        PersonalDetailedHolder holder = new PersonalDetailedHolder(parentView);
         mFirebaseProfileImageCatcher = new FirebaseProfileImageCatcher(holder);
 
         return holder;
     }
 
-    @NonNull
     @Override
-    public PersonalDetailedChildHolder onCreateChildViewHolder(@NonNull ViewGroup childViewGroup, int viewType) {
-        View childView = LayoutInflater.from(mActivity).inflate(R.layout.item_personal_detail_child, childViewGroup, false);
-
-        return new PersonalDetailedChildHolder(childView);
+    public void onBindViewHolder(PersonalDetailedHolder holder, int position) {
+        holder.bindInfoToView( mPersonalTrainers.get(position) );
     }
 
     @Override
-    public void onBindParentViewHolder(@NonNull PersonalResumeParentHolder parentViewHolder, int parentPosition, @NonNull PersonalResume parent) {
-        parentViewHolder.sendImageCatcherToFirebase(parent);
-        parentViewHolder.bindInfoToView(parent);
+    public int getItemCount() {
+        return mPersonalTrainers.size();
     }
 
-    @Override
-    public void onBindChildViewHolder(@NonNull PersonalDetailedChildHolder childViewHolder, int parentPosition, int childPosition, @NonNull PersonalDetailed child) {
-        childViewHolder.bindInfoToView(child);
-    }
+    public class PersonalDetailedHolder extends RecyclerView.ViewHolder implements FirebaseProfileImageCatcher.FirebaseProfileCallback {
 
-    public class PersonalResumeParentHolder extends ParentViewHolder<PersonalResume, PersonalDetailed> implements FirebaseProfileImageCatcher.FirebaseProfileCallback {
+        @BindView(R.id.card_view) CardView mCardView;
+        @BindView(R.id.personal_name_text_view) TextView mNameTextView;
+        @BindView(R.id.grade_text_view) TextView mGradeTextView;
+        @BindView(R.id.profile_image_view) CircleImageView mProfileCircleImageView;
+        @BindView(R.id.cref_title_text_view) TextView mCrefTitle;
+        @BindView(R.id.cref_number_text_view) TextView mCREFTextView;
+        @BindView(R.id.class_counter_text_view) TextView mClassCounterTextView;
+        @BindView(R.id.schedule_class_button) CustomButton mScheduleClassButton;
 
-        private TextView mNameTextView;
-        private TextView mGradeTextView;
-        private CircleImageView mProfileCircleImageView;
-
-
-        public PersonalResumeParentHolder(@NonNull View itemView) {
+        public PersonalDetailedHolder(View itemView) {
             super(itemView);
 
-            mNameTextView = (TextView) itemView.findViewById(R.id.personal_name_text_view);
-            mGradeTextView = (TextView) itemView.findViewById(R.id.grade_text_view);
-            mProfileCircleImageView = (CircleImageView) itemView.findViewById(R.id.profile_image_view);
+            ButterKnife.bind(this, itemView);
+
+            mCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mCREFTextView.getVisibility() == View.GONE){
+                        mCrefTitle.setVisibility(View.VISIBLE);
+                        mCREFTextView.setVisibility(View.VISIBLE);
+                        mClassCounterTextView.setVisibility(View.VISIBLE);
+                        mScheduleClassButton.setVisibility(View.VISIBLE);
+                    } else {
+                        mCrefTitle.setVisibility(View.GONE);
+                        mCREFTextView.setVisibility(View.GONE);
+                        mClassCounterTextView.setVisibility(View.GONE);
+                        mScheduleClassButton.setVisibility(View.GONE);
+                    }
+                }
+            });
         }
 
-        private void sendImageCatcherToFirebase(PersonalResume personalResume){
-            mFirebaseProfileImageCatcher.getPersonalProfilePicture(mActivity, personalResume.getPersonalKey());
+        private void sendImageCatcherToFirebase(PersonalTrainer personalTrainer){
+            mFirebaseProfileImageCatcher.getPersonalProfilePicture( mActivity, Utils.encodeEmail( personalTrainer.getEmail() ) );
         }
 
-        private void bindInfoToView(PersonalResume personalResume){
+        private void bindInfoToView(final PersonalTrainer personalTrainer){
 
-            mNameTextView.setText(personalResume.getName());
-            String grade = Utils.formatGrade(personalResume.getGrade());
+            sendImageCatcherToFirebase(personalTrainer);
+
+            mClassCounterTextView.setText(mActivity.getString(R.string.class_number,  Integer.valueOf(personalTrainer.getReviewCounter()).toString()));
+            mCREFTextView.setText(personalTrainer.getCref());
+
+
+            mScheduleClassButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ScheduleConfirmation scheduleConfirmation = new ScheduleConfirmation(mActivity, personalTrainer, mPersonalFitClass);
+                    scheduleConfirmation.makeAppointment();
+
+                }
+            });
+
+            mNameTextView.setText(personalTrainer.getName());
+            String grade = Utils.formatGrade(personalTrainer.getGrade());
             mGradeTextView.setText(grade);
         }
 
@@ -108,38 +129,7 @@ public class ExpandableBoothAdapter extends ExpandableRecyclerAdapter<PersonalRe
                 Glide.with(mActivity).load(R.drawable.head_placeholder).into(mProfileCircleImageView);
             }
         }
-    }
 
-    public class PersonalDetailedChildHolder extends ChildViewHolder<PersonalDetailed> {
-
-        private TextView mClassCounterTextView;
-        private TextView mCREFTextView;
-        private CustomButton mScheduleClassButton;
-
-        public PersonalDetailedChildHolder(@NonNull View itemView) {
-            super(itemView);
-
-            mClassCounterTextView = (TextView) itemView.findViewById(R.id.class_counter_text_view);
-            mCREFTextView = (TextView) itemView.findViewById(R.id.cref_number_text_view);
-            mScheduleClassButton = (CustomButton) itemView.findViewById(R.id.schedule_class_button);
-        }
-
-        private void bindInfoToView(PersonalDetailed personalDetailed){
-
-            mClassCounterTextView.setText(mActivity.getString(R.string.class_number, new Integer(personalDetailed.getReviewCounter()).toString()));
-            mCREFTextView.setText(personalDetailed.getCref());
-
-            final PersonalTrainer personalTrainer = mPersonalTrainers.get(getChildAdapterPosition());
-
-            mScheduleClassButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ScheduleConfirmation scheduleConfirmation = new ScheduleConfirmation(mActivity, personalTrainer, mPersonalFitClass);
-                    scheduleConfirmation.makeAppointment();
-
-                }
-            });
-        }
     }
 
 
